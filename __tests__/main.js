cb7412a0d1b21625e502196f1269ec4c7b1274eb
Patrick Scott <patrick.scott@zx-ventures.com>
2018-03-01 15:07:34 -0500

    timeout and 10 failure tests/logic

--- __tests__/main.js
+++ __tests__/main.js
@@ -1,37 +1,37 @@
 import { CircuitBreaker } from 'main'
 
 // default timeout is 5 seconds... need at least 10 for timeout tests
-// setting to 30 to be safe
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000
+// setting to 15 to be safe
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 15 * 1000
 
 describe('main', () => {
   it('exists', () => {
     expect(CircuitBreaker).toBeDefined()
-    let circuitbreaker
+    let circuitBreaker
     expect(() => {
-      circuitbreaker = new CircuitBreaker()
+      circuitBreaker = new CircuitBreaker()
     }).toThrow()
 
     expect(() => {
       let fn = jest.fn()
-      circuitbreaker = new CircuitBreaker(fn)
-      expect(typeof circuitbreaker.attempt).toBe('function')
+      circuitBreaker = new CircuitBreaker(fn)
+      expect(typeof circuitBreaker.attempt).toBe('function')
     }).not.toThrow()
 
-    expect(circuitbreaker).toBeDefined()
+    expect(circuitBreaker).toBeDefined()
   })
 
-  it('circuitbreaker.attempt success', async (done) => {
+  it('circuitBreaker.attempt success', async (done) => {
     let fn = jest.fn(() => {
       // mock async call using Promise
       return new Promise((resolve, reject) => {
         setTimeout(resolve, 100, 1)
       })
     })
-    let circuitbreaker = new CircuitBreaker(fn)
+    let circuitBreaker = new CircuitBreaker(fn)
 
     try {
-      let result = await circuitbreaker.attempt()
+      let result = await circuitBreaker.attempt()
       expect(result).toBe(1)
       done()
     } catch (e) {
@@ -39,40 +39,41 @@ describe('main', () => {
     }
   })
 
-  // it('circuitbreaker.call success', () => {
-  //   let fn = jest.fn(() => {
-  //     return new Promise(function(resolve, reject) {
-  //       setTimeout(() => {
-  //         resolve(1)
-  //       }, 100)
-  //     })
-  //   })
+  it('circuitBreaker.attempt 10 failures', async (done) => {
+    let errorMessage = 'CIRCUIT_IS_OPEN'
+    let fn = jest.fn(() => {
+      // mock async call using Promise
+      return new Promise((resolve, reject) => {
+        setTimeout(reject, 1, new Error('Server Error'))
+      })
+    })
+    let circuitBreaker = new CircuitBreaker(fn)
 
-  //   let circuitbreaker = new CircuitBreaker(fn)
-  //   expect(typeof circuitbreaker.call).toBe('function')
+    try {
+      await circuitBreaker.attempt()
+    } catch (err) {
+      if (err) console.log({msg: 'An error has occurred.', err})
+      expect(err).toEqual(new Error(errorMessage))
+      done()
+    }
+  })
 
-  //   circuitbreaker.call(fn, (err, result) => {
-  //     if (err) console.log(err)
-  //     expect(result).toBe(1)
-  //     done()
-  //   })
+  it('circuitBreaker.attempt timeout', async (done) => {
+    let errorMessage = 'Request taking too long'
+    let fn = jest.fn(() => {
+      // mock async call using Promise
+      return new Promise((resolve, reject) => {
+        setTimeout(resolve, 15 * 1000, 1)
+      })
+    })
+    let circuitBreaker = new CircuitBreaker(fn)
 
-  //   expect(fn).toBeCalled()
-  // })
-
-  // it('circuitbreaker.call returns error ‘CIRCUIT_IS_OPEN’ when ten attempts have failed', (done) => {
-  //   let circuitbreaker = new CircuitBreaker()
-  //   expect(typeof circuitbreaker.call).toBe('function')
-  //   let fn = jest.fn(() => {
-  //     throw new Error('ERROR')
-  //   })
-
-  //   circuitbreaker.call(fn, (err, result) => {
-  //     if (err) console.error(err)
-  //     expect(err).toEqual(new Error('CIRCUIT_IS_OPEN'))
-  //     done()
-  //   })
-
-  //   expect(fn).toBeCalled()
-  // })
+    try {
+      await circuitBreaker.attempt()
+    } catch (err) {
+      if (err) console.log({msg: 'An error has occurred.', err})
+      expect(err).toEqual(new Error(errorMessage))
+      done()
+    }
+  })
 })
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
+import debug from 'debug'
 import EventEmitter from 'events'
+
+const log = debug('circuit-breaker-await-async')
+
+const states = {
+  CLOSED: 'CLOSED',
+  OPEN: 'OPEN',
+  HALF_OPEN: 'HALF_OPEN'
+}
+
+const errors = {
+  CIRCUIT_IS_OPEN: 'CIRCUIT_IS_OPEN'
+}
 
 export class CircuitBreaker extends EventEmitter {
   constructor(fn) {
@@ -13,21 +26,53 @@ export class CircuitBreaker extends EventEmitter {
     this.callTimeoutMs = 1 * 1000
     this.resetTimeoutMs = 10 * 1000
 
-    this.state = {
-      currentAttempts: 0
+    this.currentAttempt = 0
+    this.state = states.CLOSED
+
+    let closedStateAttempt = async () => {
+      if (this.currentAttempt < this.maxFailures) {
+        try {
+          this.currentAttempt++
+          log(`attempt ${this.currentAttempt}`)
+          let result = await this.fn()
+          if (result) {
+            this.emit('circuitbreaker.attempt.succeeded', 1)
+          } else {
+            throw new Error('Unexpected result')
+          }
+        } catch (e) {
+          log(e)
+          this.emit('circuitbreaker.attempt')
+        }
+      } else {
+        log('Calls to your function have failed 10 times in a row')
+        this.emit('circuitbreaker.trip')
+        this.emit('circuitbreaker.attempt')
+      }
+    }
+
+    let openStateAttempt = () => {
+      log('circuitBreaker is open')
+      this.emit('circuitbreaker.attempt.rejected', new Error(errors.CIRCUIT_IS_OPEN))
     }
 
     this.on('circuitbreaker.attempt', async () => {
-      if (this.state.currentAttempts < this.maxFailures) {
-        let result = await this.fn()
-        if (result) {
-          this.emit('circuitbreaker.attempt.succeeded', 1)
-        }
+      switch (this.state) {
+      case states.CLOSED:
 
-        this.currentAttempts++
-      } else {
-        this.emit('circuitbreaker.reset')
+        closedStateAttempt()
+
+        break
+
+      case states.OPEN:
+
+        openStateAttempt()
       }
+    })
+
+    this.on('circuitbreaker.trip', async () => {
+      log('tripping circuitbreaker')
+      this.state = states.OPEN
     })
   }
 
@@ -39,8 +84,12 @@ export class CircuitBreaker extends EventEmitter {
         resolve(result)
       })
 
+      this.on('circuitbreaker.attempt.rejected', (err) => {
+        reject(err)
+      })
+
       setTimeout(() => {
-        console.log('timeout')
+        log('attempt has reached timeout')
         reject(new Error('Request taking too long'))
       }, this.resetTimeoutMs)
     })
